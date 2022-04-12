17aab12a0507ae9416abf4b6899195ec50899d0e
Rafael Linnankoski <rafaellinnankoski@gmail.com>
2022-04-12 11:34:35 +0900

    Improve SpendByCategoryTable (add sorting and refactors)

--- client/src/components/SpendByCategoryTable.tsx
+++ client/src/components/SpendByCategoryTable.tsx
@@ -1,6 +1,61 @@
+import _ from "lodash";
+import { useReducer } from "react";
 import { Header, Table } from "semantic-ui-react";
 import { CustomPieChartData } from "../types";
 import { getCountOfDaysInMonth } from "../utils";
+
+enum SortActionKind {
+  CHANGE_SORT = "CHANGE_SORT",
+}
+
+interface SortAction {
+  type: SortActionKind;
+  column: string;
+}
+
+enum SortDirection {
+  ascending = "ascending",
+  descending = "descending",
+}
+
+interface SortState {
+  column: string | undefined;
+  data: TableDataType;
+  direction: SortDirection | undefined;
+}
+
+type TableDataType = {
+  name: string;
+  budget: number;
+  weeklyBudget: number;
+  spend: number | undefined;
+  remainingBudget: number;
+  remainingWeeklyBudget: number;
+}[];
+
+function tableReducer(state: SortState, action: SortAction): SortState {
+  switch (action.type) {
+    case "CHANGE_SORT":
+      if (state.column === action.column) {
+        return {
+          ...state,
+          data: state.data.slice().reverse(),
+          direction:
+            state.direction === "ascending"
+              ? SortDirection.descending
+              : SortDirection.ascending,
+        };
+      }
+
+      return {
+        column: action.column,
+        data: _.sortBy(state.data, [action.column]),
+        direction: SortDirection.ascending,
+      };
+    default:
+      throw new Error();
+  }
+}
 
 interface SpendByCategoryTableProps {
   categoryData: CustomPieChartData[];
@@ -11,6 +66,38 @@ const SpendByCategoryTable = ({
   categoryData,
   entrySumData,
 }: SpendByCategoryTableProps) => {
+  const today = new Date();
+  const weekDivider =
+    getCountOfDaysInMonth(today.getFullYear(), today.getMonth()) / 7;
+  const getWeeklyBudget = (budget: number): number =>
+    Math.floor(budget / weekDivider);
+
+  const tableData: TableDataType = categoryData.map((category) => {
+    const expenseValue = entrySumData.find(
+      (data) => data.id === category.id
+    )?.value;
+    const remainingBudget = expenseValue
+      ? category.value - expenseValue
+      : category.value;
+    return {
+      name: category.id,
+      budget: category.value,
+      weeklyBudget: getWeeklyBudget(category.value),
+      spend: expenseValue,
+      remainingBudget: remainingBudget,
+      remainingWeeklyBudget: getWeeklyBudget(remainingBudget),
+    };
+  });
+  const initialState = {
+    column: undefined,
+    data: tableData,
+    direction: undefined,
+  };
+
+  const [state, dispatch] = useReducer(tableReducer, initialState);
+
+  const { column, data, direction }: SortState = state;
+
   const categoriesTotal = categoryData.reduce(
     (sum, cat) => (sum += cat.value),
     0
@@ -20,59 +107,111 @@ const SpendByCategoryTable = ({
     0
   );
 
-  const today = new Date();
-  const weekDivider =
-    getCountOfDaysInMonth(today.getFullYear(), today.getMonth()) / 7;
-  const getWeeklyBudget = (budget: number): number =>
-    Math.floor(budget / weekDivider);
-
   return (
     <>
       <Header as="h3">Spend by category</Header>
       <div style={{ overflowX: "scroll" }}>
-        <Table basic="very" unstackable singleLine>
+        <Table basic="very" sortable unstackable>
           <Table.Header>
             <Table.Row>
-              <Table.HeaderCell>Expenses</Table.HeaderCell>
-              <Table.HeaderCell>Budget</Table.HeaderCell>
-              <Table.HeaderCell>/ week</Table.HeaderCell>
-              <Table.HeaderCell>Spent</Table.HeaderCell>
-              <Table.HeaderCell>+/-</Table.HeaderCell>
-              <Table.HeaderCell>/ week</Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === "name" ? direction : undefined}
+                onClick={() =>
+                  dispatch({ type: SortActionKind.CHANGE_SORT, column: "name" })
+                }
+              >
+                Expenses
+              </Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === "budget" ? direction : undefined}
+                onClick={() =>
+                  dispatch({
+                    type: SortActionKind.CHANGE_SORT,
+                    column: "budget",
+                  })
+                }
+              >
+                Budget
+              </Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === "weeklyBudget" ? direction : undefined}
+                onClick={() =>
+                  dispatch({
+                    type: SortActionKind.CHANGE_SORT,
+                    column: "weeklyBudget",
+                  })
+                }
+              >
+                / week
+              </Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === "spend" ? direction : undefined}
+                onClick={() =>
+                  dispatch({
+                    type: SortActionKind.CHANGE_SORT,
+                    column: "spend",
+                  })
+                }
+              >
+                Spent
+              </Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={column === "remainingBudget" ? direction : undefined}
+                onClick={() =>
+                  dispatch({
+                    type: SortActionKind.CHANGE_SORT,
+                    column: "remainingBudget",
+                  })
+                }
+              >
+                +/-
+              </Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={
+                  column === "remainingWeeklyBudget" ? direction : undefined
+                }
+                onClick={() =>
+                  dispatch({
+                    type: SortActionKind.CHANGE_SORT,
+                    column: "remainingWeeklyBudget",
+                  })
+                }
+              >
+                / week
+              </Table.HeaderCell>
             </Table.Row>
           </Table.Header>
           <Table.Body>
-            {categoryData
-              .sort((a, b) => a.id.localeCompare(b.id))
-              .map((category) => {
-                const expenseValue = entrySumData.find(
-                  (data) => data.id === category.id
-                )?.value;
-                const remainingBudget = expenseValue
-                  ? category.value - expenseValue
-                  : category.value;
+            {data.map(
+              ({
+                name,
+                budget,
+                weeklyBudget,
+                spend,
+                remainingBudget,
+                remainingWeeklyBudget,
+              }) => {
                 return (
                   <Table.Row
+                    key={name}
                     negative={remainingBudget < 0 ? true : false}
                     warning={
-                      remainingBudget > 0 &&
-                      remainingBudget < category.value / 5
+                      remainingBudget > 0 && remainingBudget < budget / 5
                         ? true
                         : false
                     }
-                    positive={
-                      remainingBudget > category.value / 2 ? true : false
-                    }
+                    positive={remainingBudget > budget / 2 ? true : false}
                   >
-                    <Table.Cell collapsing>{category.id}</Table.Cell>
-                    <Table.Cell>{category.value}</Table.Cell>
-                    <Table.Cell>{getWeeklyBudget(category.value)}</Table.Cell>
-                    <Table.Cell>{expenseValue}</Table.Cell>
+                    <Table.Cell>{name}</Table.Cell>
+                    <Table.Cell>{budget}</Table.Cell>
+                    <Table.Cell>{weeklyBudget}</Table.Cell>
+                    <Table.Cell>{spend}</Table.Cell>
                     <Table.Cell>{remainingBudget}</Table.Cell>
-                    <Table.Cell>{getWeeklyBudget(remainingBudget)}</Table.Cell>
+                    <Table.Cell>{remainingWeeklyBudget}</Table.Cell>
                   </Table.Row>
                 );
-              })}
+              }
+            )}
           </Table.Body>
           <Table.Footer>
             <Table.Row>
